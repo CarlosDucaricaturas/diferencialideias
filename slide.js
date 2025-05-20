dc7154422eb95c6d4a9f449c10b8e9d377c06b2c
Carlos Edu Florentino <96946598+CarlosDucaricaturas@users.noreply.github.com>
2025-05-19 22:55:09 -0300

    Add files via upload

--- slide.js
+++ slide.js
@@ -1,17 +1,22 @@
 var radio = document.querySelector('.manual-btn')
 document.getElementById('radio1').cheked = true
 var cont = 1
-
+var cont = 3
 
 setInterval(() => {
     proximapng()
-}, 3000)
+}, 2500)
 
 function proximapng(){
   cont++
-  if(cont > 4){
+ 
+    if(cont > 4) { 
       cont = 1
-  }
+      
+  
+     
+      
+      }
   
   document. getElementById('radio'+cont).checked = true
-}
+  }
